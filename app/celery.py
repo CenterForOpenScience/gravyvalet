2d0aa05e58934c05efdcaf5d0333b467aa0a460d
John Tordoff <>
2024-03-13 09:59:57 -0400

    use a celery task for listener and change the message detriment the message body

--- app/celery.py
+++ app/celery.py
@@ -3,31 +3,12 @@ from kombu import (
     Exchange,
     Queue,
 )
-
 from app import settings
 
-
-deactivated_user_route = Queue(
+account_status_change_queue = Queue(
     settings.EXCHANGE_NAME,
     Exchange(settings.EXCHANGE_NAME),
-    routing_key=settings.DEACTIVATED_ROUTING_KEY,
 )
-reactivated_user_route = Queue(
-    settings.EXCHANGE_NAME,
-    Exchange(settings.EXCHANGE_NAME),
-    routing_key=settings.REACTIVATED_ROUTING_KEY,
-)
-merged_user_route = Queue(
-    settings.EXCHANGE_NAME,
-    Exchange(settings.EXCHANGE_NAME),
-    routing_key=settings.MERGED_ROUTING_KEY,
-)
-
-account_status_change_queues = [
-    deactivated_user_route,
-    reactivated_user_route,
-    merged_user_route,
-]
 
 # Assuming 'app' is your Celery app instance
 app = Celery()
@@ -36,8 +17,6 @@ app.conf.update(
     task_default_exchange=settings.EXCHANGE_NAME,
     task_default_exchange_type="direct",
     task_queues=(
-        deactivated_user_route,
-        reactivated_user_route,
-        merged_user_route,
+        account_status_change_queue,
     ),
 )
