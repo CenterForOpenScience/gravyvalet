288d9450740ed6aae48a27913c94fa591e5cc43d
Yuhuai Liu <yuhuai@cos.io>
2024-08-21 04:44:58 -0400

    CR Followup

--- addon_service/external_storage_service/models.py
+++ addon_service/external_storage_service/models.py
@@ -3,7 +3,6 @@ from enum import (
     auto,
 )
 
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 from addon_service.abstract.external_storage.models import ExternalService
@@ -53,16 +52,19 @@ class ExternalStorageService(ExternalService):
         null=True,
         blank=True,
     )
-    int_supported_features = ArrayField(
-        models.IntegerField(validators=[validate_supported_features]),
-        null=True,
-        blank=True,
+    int_supported_features = models.IntegerField(
+        validators=[validate_supported_features], null=True
     )
 
     @property
     def supported_features(self) -> list[SupportedFeatures]:
         """get the enum representation of int_supported_features"""
-        return [SupportedFeatures(item) for item in self.int_supported_features]
+        return SupportedFeatures(self.int_supported_features)
+
+    @supported_features.setter
+    def authorized_capabilities(self, new_supported_features: SupportedFeatures):
+        """set int_authorized_capabilities without caring its int"""
+        self.int_supported_features = new_supported_features.value
 
     class Meta:
         verbose_name = "External Storage Service"
