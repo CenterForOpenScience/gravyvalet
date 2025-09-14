5e18636e8e51fcff1aaa6c1e2616119900460cf5
Yuhuai Liu <yuhuai@cos.io>
2025-09-13 22:43:05 -0400

    fix some issues so that API returns external redirect service

--- addon_service/external_service/redirect/serializers.py
+++ addon_service/external_service/redirect/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework_json_api import serializers
 from rest_framework_json_api.utils import get_resource_type_from_model
 
+from addon_service.addon_imp.models import AddonImpModel
 from addon_service.common import view_names
-
+from addon_service.common.serializer_fields import DataclassRelatedDataField
 from addon_service.external_service.serializers import ExternalServiceSerializer
 
 from .models import ExternalRedirectService
@@ -13,6 +14,15 @@ RESOURCE_TYPE = get_resource_type_from_model(ExternalRedirectService)
 
 class ExternalRedirectServiceSerializer(ExternalServiceSerializer):
     """api serializer for the `ExternalRedirectService` model"""
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name=view_names.detail_view(RESOURCE_TYPE)
+    )
+
+    addon_imp = DataclassRelatedDataField(
+        dataclass_model=AddonImpModel,
+        related_link_view_name=view_names.related_view(RESOURCE_TYPE),
+    )
 
     redirect_url = serializers.CharField(
         read_only=True,
